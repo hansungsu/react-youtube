7b447a2100d61f9db380bb395b5b906e512977a1
hansungsoo <thepub03@gmail.com>
2021-07-26 13:45:16 +0900

    fetch 디자인 패턴대로 나누기

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,15 @@ import VideoList from './components/video_list/video_list';
 import VideoSearch from './components/video_search/video_search';
 
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([])
-  
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyD9G5NXtNgrEz_9uXf9NwV6ZHokJBqY_a0`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube.search(query).then(console.log())
   }
 
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD9G5NXtNgrEz_9uXf9NwV6ZHokJBqY_a0", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, []) //[] 아무것도 전달하지 않으면 '한번만', //변수가 들어가면 변수가 바뀔때만 state가 변경된다.
+  useEffect(()=>{
+    youtube.mostPopular().then(console.log())
+  })
 
   return (
     <>
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Youtube from './service/youtube';
 
+const youtube = new Youtube('asdf');
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
